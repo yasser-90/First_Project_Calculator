8cc40e16f6eeb95fd11e031e273716142b118596
yasser mohammad <yassermh1990@gmail.com>
2023-03-08 06:20:09 -1000

    change the files

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,5 @@
-def subtract(x, y):
-    return x - y
-
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
+from Operations  import opeartion as oper
+#  User Select  the operation
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -27,23 +13,23 @@ while True:
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            num1 = float(input("Enter first number: "))# user enter first number (operand)
+            num2 = float(input("Enter second number: "))# user enter second number (operand)
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
         print("Result of Operation = ")
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", oper.add(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(num1, "-", num2, "=", oper.subtract(num1, num2))
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(num1, "*", num2, "=", oper.multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            print(num1, "/", num2, "=",oper.divide(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
---
+++ Operations.py
@@ -0,0 +1,15 @@
+class opeartion:
+    def subtract(x, y):
+        return x - y
+
+    # This function adds two numbers
+    def add(x, y):
+        return x + y
+
+    # This function multiplies two numbers
+    def multiply(x, y):
+        return x * y
+
+    # This function divides two numbers
+    def divide(x, y):
+        return x / y
