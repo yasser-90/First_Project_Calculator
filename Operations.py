7f1593f9915248f60e832ab93620fb8f9f4edfe7
ZubaidaKhalidS4A <126612785+ZubaidaKhalidS4A@users.noreply.github.com>
2023-03-14 19:25:31 +0300

    Update Operations.py

--- Operations.py
+++ Operations.py
@@ -1,4 +1,3 @@
-import math
 class opeartion:
     def subtract(x, y):
         return x - y
@@ -14,35 +13,3 @@ class opeartion:
     # This function divides two numbers
     def divide(x, y):
         return x / y
-    def power(x,y):
-        return x ** y
-    
-    def cosine(x):
-        angle_radians = math.radians(x)  #used to convert an angle from degrees to radians.                   بالتقدير الدائري.
-        return math.cos(angle_radians)
-     
-    def sine(x):
-        angle_radians = math.radians(x)
-        return math.sin(angle_radians)
-    
-    def tanget(x):
-        angle_radians = math.radians(x)
-        return math.tan(angle_radians)
-     
-    def logartim(x):
-        return math.log(x)
-    
-    def pai(x):
-        return math.pi*(x)
-    
-    def Square(x):
-        return math.sqrt(x)
-    
-    def cube_root(x):
-        return math.pow(x,1/3)
-        
-    def max_num(x,y):
-        if x > y:
-            return x
-        else:
-            return y
