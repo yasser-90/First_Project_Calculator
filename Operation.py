a80253f5e9e1011e3727fa4c1c35718986896c36
SamanNaifS4B <126913884+SamanNaifS4B@users.noreply.github.com>
2023-03-15 23:50:54 +0300

    Add files via upload

---
+++ Operation.py
@@ -0,0 +1,19 @@
+class opeartion:
+    def subtract(x, y):
+        return x - y
+
+    # This function adds two numbers
+    def add(x, y):
+        return x + y
+
+    # This function multiplies two numbers
+    def multiply(x, y):
+        return x * y
+
+    # This function divides two numbers
+    def divide(x, y):
+        return x / y
+    def power(x, y):
+       return x ** y
+    def cbrt(x):
+       return x ** (1/3)
