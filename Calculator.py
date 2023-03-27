475756975a0ab74c4c35e1941d863121c791e3ba
HAGER <hager.cs500@student.uomosul.edu.iq>
2023-03-28 01:49:46 +0300

    kkcjk

--- Calculator.py
+++ Calculator.py
@@ -8,7 +8,7 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter   choice(1/2/3/4): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
